a261831229e341dbb9653141056c55ef912f9cac
ZZ1518-CODER <64011516+ZZ1518-CODER@users.noreply.github.com>
2020-07-09 00:07:39 +0800

    Update mergerSortBU.hpp

--- MyCplusLearning/MyCplusLearning/mergerSortBU.hpp
+++ MyCplusLearning/MyCplusLearning/mergerSortBU.hpp
@@ -10,13 +10,53 @@
 #define mergerSortBU_hpp
 
 #include <iostream>
+
 template <class T>
-void mergerSortBU(T arr[], int n) {
-    for(int i = 1; i < n; i++) {
-        for(int j = 0; j < n; j += i) {
-            
+void __mergeBU(T arr[], int left, int right, int mid) {
+    T* arrTmp = new T[right - left + 1];
+    for(int i = left; i <= right; i++) {
+        arrTmp[i - left] = arr[i];
+    }
+    int l = left, r = mid + 1;
+    for(int k = left; k <= right; k++){
+        if(l > mid) {
+            arr[k] = arrTmp[r - left];
+            r++;
+        }
+        else if(r > right) {
+            arr[k] = arrTmp[l - left];
+            l++;
+        }
+        else if(arr[l] > arr[r]) {
+            arr[k] = arrTmp[r - left];
+            r++;
+        }
+        else {
+            arr[k] = arrTmp[l - left];
+            l++;
         }
     }
+    delete [] arrTmp;
+}
+
+template <class T>
+void mergerSortBU(T arr[], int n) {
+    /*for(int i = 1; i < n; i *= 2) {
+        for(int j = 0; j < n - i; j += i * 2) {
+            int left = j;
+            int right = std::min(j + i * 2 - 1, n - 1);
+            int mid = j + i - 1;
+            __mergeBU(arr, left, right, mid);
+        }
+    }*/
+    
+    
+
+    for( int sz = 1; sz < n ; sz += sz )
+        for( int i = 0 ; i < n - sz ; i += sz+sz )
+            // 对于arr[mid] <= arr[mid+1]的情况,不进行merge
+            if( arr[i+sz-1] > arr[i+sz] )
+                __merge(arr, i, i+sz-1, std::min(i+sz+sz-1,n-1) );
 }
 
 #endif /* mergerSortBU_hpp */
